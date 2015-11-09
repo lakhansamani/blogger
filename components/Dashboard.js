d9d483457edd7dbe7d7b41fa1ee340343464c83f
lakhansamani <lakhan.m.samani@gmail.com>
2015-11-09 16:26:33 +0530

    Adds form for writing post

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -7,10 +7,13 @@ var Button = belle.Button;
 var Card=belle.Card
 const Dashboard = React.createClass({
   getInitialState: function() {
-    return {value: 'Type some *markdown* here!'};
+    return {value: 'Type some *markdown* here!',title:''};
   },
   handleChange: function() {
     this.setState({value: this.refs.textarea.value});
+  },
+  handleTitleChange:function(){
+    this.setState({title:this.ref.title.value})
   },
   rawMarkup: function() {
     return { __html: marked(this.state.value, {sanitize: true}) };
@@ -18,14 +21,17 @@ const Dashboard = React.createClass({
   render: function() {
     return (
       <div className="row">
-      <Card className="columns five offset-by-six">
+      <Card className="columns eight offset-by-five">
+      <h3>Blog post title</h3>
+      <input ref="title" placeholder="Blog post title" className="form-item" ref="title" onChange={this.handleTitleChange}/>
       <div className="MarkdownEditor">
-        <h3>Input</h3>
-        <textarea className="form-item"
+        <h3>Blog Post</h3>
+        <textarea className="form-item txt"
           onChange={this.handleChange}
           ref="textarea"
-          defaultValue={this.state.value} />
-        <h3>Output</h3>
+          defaultValue={this.state.value}/>
+        <h3>Rendering your post</h3>
+        <h4>{this.state.title}</h4>
         <div
           className="content"
           dangerouslySetInnerHTML={this.rawMarkup()}
