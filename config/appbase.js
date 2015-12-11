c0330f6d728bd51a7f294ac2af301a9c5b6645aa
lakhansamani <lakhan.m.samani@gmail.com>
2015-12-11 11:27:46 +0530

    Test server

---
+++ config/appbase.js
@@ -0,0 +1,8 @@
+var Appbase=require('appbase-js')
+var appbase=new Appbase({
+  url: 'https://scalr.api.appbase.io',
+  appname: 'blog',
+  username: 'h5aIUPBPO',
+  password: '348cee55-1054-4021-ad35-15f93016e876'
+})
+module.exports=appbase;
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
+var path = require('path');
 var webpack = require('webpack');
-module.exports = {
-    entry: [
-      'webpack/hot/only-dev-server',
-      "./js/app.js"
-    ],
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.js?$/, loaders: ['babel'], exclude: /node_modules/ },
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin()
-    ]
 
-};
+module.exports = {
+  devtool: 'eval',
+
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './js/app.js'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+        { test: /\.js?$/, loaders: ['babel'], exclude: /node_modules/ },
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+        { test: /\.css$/, loader: "style!css" }
+    ]
+  }
+}
