b710f95bb001e1ea2d52bb2d3b3d2698ffd64bb3
lakhansamani <lakhan.m.samani@gmail.com>
2015-12-11 20:24:27 +0530

    Adds example blog

---
+++ config.js
@@ -0,0 +1,7 @@
+var blogger_username="lakhan.m.samani@gmail.com";
+var credentials={
+  url: 'https://scalr.api.appbase.io',
+  appname: 'blog',
+  username: 'h5aIUPBPO',
+  password: '348cee55-1054-4021-ad35-15f93016e876'
+}
