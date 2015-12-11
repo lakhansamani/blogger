3d7950b6f4c73ac903d724ee77258e678979f929
lakhansamani <lakhan.m.samani@gmail.com>
2015-12-11 21:26:23 +0530

    Adds instruction and example

--- components/Landing.js
+++ components/Landing.js
@@ -10,7 +10,20 @@ const Landing = React.createClass({
       		<h3> Blogger </h3>
       		<p>
       			Blogger is live blogging platform created using <a href="https://facebook.github.io/react/" target="_blank">React</a> and <a href="https://appbase.io" target="_blank"> Appbase </a>
+            Blogger is an open source project you can fork it on <a href="https://github.com/lakhansamani/blogger" target="_blank"> Github</a> and build your own blogging platfrom.
       		</p>
+          <h5>Get Started With blogger </h5>
+          <ul>
+            <li> Create account on blogger</li>
+            <li> Download <a href="/example/example.zip">client example</a> developed using React and appbase</li>
+            <li> Configure example with your blogger username </li>
+            <pre>
+                cd example<br/>
+                vim config.js
+            </pre>
+            <li> Change value of <pre>blogger_username</pre> </li>
+            <li> Host your blog on github, if it is your personal blog create repo with username.github.io else if its blog for your project you can host it in `gh-pages` branch</li>
+          </ul>
       	</Card>
        </div>
     )
